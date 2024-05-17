eb57c1b9fe87e9a71ff7b94b1f2c70ca536dbcb1
marcellusmbarbosa <marcellusmbarbosa@gmail.com>
2024-05-17 20:40:39 -0300

    Callback funcion e events

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 const randomNumber = Math.round(Math.random() *10)
 
 let xAttempts = 1
 
-
-
-function handleClick(event){
+//Eventos
+//função callback
+function handleTryClick(event){
   event.preventDefault()
 
   const inputNumber = document.querySelector("#inputNumber")
 
+  inputNumber.value = ""
   xAttempts++
 
   if (Number(inputNumber.value) == randomNumber){
-    document.querySelector(".screen1").classList.add("hide")
-    document.querySelector(".screen2").classList.remove("hide")
+    screen1.classList.add("hide")
+    screen2.classList.remove("hide")
     
-    document.querySelector(".screen2 h2").innerText = `Você acertou em ${xAttempts} tentativas`
+    screen2.querySelector("h2").innerText = `Você acertou em ${xAttempts} tentativas`
   }
   console.log(xAttempts)
 }
+
+
+function handleResetClick(){  
+  screen1.classList.remove("hide")
+  screen2.classList.add("hide")
+  xAttempts = 1
+}
+
+btnTry.addEventListener("click", handleTryClick)
+btnReset.addEventListener("click", handleResetClick)
